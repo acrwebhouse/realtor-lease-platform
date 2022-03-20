972d0255d1280d2435cec02a6ff7e0da4c266a81
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-03-20 20:15:38 +0800

    add select

--- src/Components/HouseList.js
+++ src/Components/HouseList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Form, Button, Modal, Input, Select, Divider, Row, Col, Checkbox, message} from "antd";
+import {Tag, Button, Modal, Input, Select, Divider, Row, Col, Checkbox, message} from "antd";
 import { PlusOutlined } from '@ant-design/icons';
 import {HouseListAxios} from './axiosApi'
 // import { UploadOutlined } from '@ant-design/icons';
@@ -113,42 +113,110 @@ const HousesList = () => {
         ).then( (response) => console.log(response))
         .catch( (error) => message.error(`${error}`, 2))
     }
+    
 
+    const children = [];
+    for (let i = 10; i < 36; i++) {
+        children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+    }
+
+    function handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
+    const cityOptions = [{ value: '基隆市' }, { value: '台北市' }, { value: '新北市' }, { value: '桃園縣' }, { value: '新竹市' }, { value: '新竹縣' }, { value: '苗栗縣' }, { value: '台中市' }, { value: '彰化縣' }, { value: '南投縣' }, { value: '雲林縣' }, { value: '嘉義市' }, { value: '嘉義縣' }, { value: '台南市' }, { value: '高雄市' }, { value: '屏東縣' }, { value: '台東縣' }, { value: '花蓮縣' }, { value: '宜蘭縣' }, { value: '澎湖縣' }, { value: '金門縣' }, { value: '連江縣' }];
 
     return (
 
         <div>
-            價格：
-            <Checkbox value="0-5000">
-                0-5000
-            </Checkbox>
-            <Checkbox value="5000-10000">
-                5000-10000
-            </Checkbox>
-            <Checkbox value="10000-20000">
-                10000-20000
-            </Checkbox>
-            <Checkbox value="20000-30000">
-                20000-30000
-            </Checkbox>
-            <Checkbox value="30000-40000">
-                30000-40000
-            </Checkbox>
-            <Checkbox value="40000-9999999">
-                40000 以上
-            </Checkbox>
-            <br/><br/>
-            價格：
-            <Select value="選擇價格" style={{
+
+            <Select placeholder="縣市" options={cityOptions} style={{
                             width: 150,
                         }}>
-                <Option value="0-5000">0-5000</Option>
-                <Option value="5000-10000">5000-10000</Option>
-                <Option value="10000-20000">10000-20000</Option>
-                <Option value="20000-30000">20000-30000</Option>
-                <Option value="30000-40000">30000-40000</Option>
-                <Option value="40000-9999999">40000 以上</Option>
+                
             </Select>
+
+            <Select  mode="multiple" allowClear placeholder="區域" onChange={handleChange} style={{
+                            width: 150,
+                        }}>
+                <Option value="pet">文山區</Option>
+                <Option value="smoke">大安區</Option>           
+            </Select>
+
+            <Select placeholder="類型" style={{
+                            width: 150,
+                        }}>
+                <Option value="typeOfRental1">整層住家</Option>
+                <Option value="typeOfRental2">獨立套房</Option>
+                <Option value="typeOfRental3">分租套房</Option>
+                <Option value="typeOfRental4">雅房</Option>
+            </Select>
+
+            <Select placeholder="租金" style={{
+                            width: 150,
+                        }}>
+                <Option value="rentLimit5000">0-5000</Option>
+                <Option value="rentLimit10000">5000-10000</Option>
+                <Option value="rentLimit20000">10000-20000</Option>
+                <Option value="rentLimit30000">20000-30000</Option>
+                <Option value="rentLimit40000">30000-40000</Option>
+                <Option value="rentNoLimit">40000 以上</Option>
+                <Option value="rentCustom">自訂租金範圍</Option>
+            </Select>
+            
+            <Select placeholder="格局" style={{
+                            width: 150,
+                        }}>
+                <Option value="room1">1 房</Option>
+                <Option value="room2">2 房</Option>
+                <Option value="room3">3 房</Option>
+                <Option value="room4Up">4 房以上</Option>
+            </Select>
+
+            <Select placeholder="型態" style={{
+                            width: 150,
+                        }}>
+                <Option value="buildingType1">公寓</Option>
+                <Option value="buildingType2">電梯大樓</Option>
+                <Option value="buildingType3">透天</Option>
+            </Select>
+
+            <Select placeholder="坪數" style={{
+                            width: 150,
+                        }}>
+                <Option value="pingLimit10">10 坪以下</Option>
+                <Option value="pingLimit20">10 ~ 20 坪</Option>
+                <Option value="pingLimit30">20 ~ 30 坪</Option>
+                <Option value="pingLimit40">30 ~ 40 坪</Option>
+                <Option value="pingLimit50">40 ~ 50 坪</Option>
+                <Option value="pingCustom">自訂坪數範圍</Option>
+            </Select>
+
+            <Select placeholder="樓層" style={{
+                            width: 150,
+                        }}>
+                <Option value="floorLimit1">1 層</Option>
+                <Option value="floorLimit6">2 層 ~ 6 層</Option>
+                <Option value="floorLimit12">6 層 ~ 12 層</Option>
+                <Option value="floorNoLimit">12 層以上</Option>
+                <Option value="floorCustom">自訂樓層範圍</Option>
+            </Select>
+            <br/><br/>
+            <Select  mode="multiple" allowClear placeholder="特色" onChange={handleChange} style={{
+                            width: 150,
+                        }}>
+                <Option value="pet">可養寵物</Option>
+                <Option value="smoke">可吸菸</Option>
+                <Option value="cook">可開伙</Option>
+                <Option value="manager">有管理員</Option>
+                <Option value="parking">有車費</Option>
+                <Option value="garbage">倒垃圾服務</Option>
+                
+            </Select>
+
+            
+
+
 
             <br/><br/>
             <Button type="primary" onClick={getHousesList}>
