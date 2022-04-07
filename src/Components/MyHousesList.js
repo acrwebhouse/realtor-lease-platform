7c9b763b7b9e697a5e0f8fec6c9974f8b09ce915
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-04-08 00:14:35 +0800

    fix bug

--- src/Components/MyHousesList.js
+++ src/Components/MyHousesList.js
@@ -1,23 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {Table, Tag, Radio, Button, Image, Input, Select, Divider, Row, Col, Span, message, Alert, Space} from "antd";
-import { PlusOutlined } from '@ant-design/icons';
-import {HouseListAxios} from './axiosApi'
-import { defaultIconPrefixCls } from 'antd/lib/config-provider';
-// import { UploadOutlined } from '@ant-design/icons';
-// import { Text, StyleSheet } from "react-native";
+import React from 'react';
 import HousesList from "./HousesList";
 import cookie from 'react-cookies'
 import jwt_decode from "jwt-decode";
 
 
-
-const xToken = cookie.load('x-token')
-const decodedToken = jwt_decode(xToken);
-console.log('===decodedToken====',decodedToken)
-
 const MyHousesList = () => {
+    const xToken = cookie.load('x-token')
+    const decodedToken = jwt_decode(xToken);
     return (
-
         <div>
             <HousesList owner={decodedToken.id}></HousesList>
         </div>
