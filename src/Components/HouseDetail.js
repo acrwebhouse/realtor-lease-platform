b2ba874821228a7e7c13b6c265672fd9ac72b396
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-04-12 21:30:22 +0800

    houseDetail

--- src/Components/HouseDetail.js
+++ src/Components/HouseDetail.js
@@ -3,9 +3,31 @@ import {Table, Tag, Radio, Button, Image, Input, Select, Divider, Row, Col, Span
 import {
     useParams
   } from "react-router-dom";
+  import {HouseAxios} from './axiosApi'
+  const houseListUrl = 'house/getHouse'
 
 const HouseDetail = () => {
     const { id } = useParams();
+    const [init, setInit] = useState(true);
+    const [house, setHouse] = useState(true);
+
+    const getHouse = () => {
+        let reqUrl = `${houseListUrl}?id=${id}&&isDelete=false`
+        HouseAxios.get(
+            reqUrl,{}
+        )
+        .then( (response) => {
+            setHouse(response)
+        })
+        .catch( (error) => message.error(error, 3))
+    }
+
+    useEffect(() => {
+        if (init) {
+            setInit(false)
+            getHouse()
+        }
+    }, )
     return (
     
         <div>
@@ -16,7 +38,7 @@ const HouseDetail = () => {
             <Col xs={24} sm={8} md={8} lg={8} xl={8}></Col>
                 <Col xs={24} sm={8} md={8} lg={8} xl={8}style={{
                             textAlign: 'center',
-                        }}>{id}</Col>
+                        }}>{JSON.stringify(house)}</Col>
                 <Col xs={24} sm={8} md={8} lg={8} xl={8}></Col>   
             </Row>
         </div>
