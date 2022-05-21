9040bcbcec903eb8bef3207bf7076ef20b670f03
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-05-21 21:40:14 +0800

    deploy sh temp

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-sudo docker stop web-login-register
-sudo docker rm web-login-register
+sudo docker stop realtor-lease-platform
+sudo docker rm realtor-lease-platform
 rm -rf code
 mkdir code
 cd code
-git clone git@github.com:acrwebhouse/loginRegister.git
-cd loginRegister
+git clone git@github.com:acrwebhouse/realtor-lease-platform.git
+cd realtor-lease-platform
 git checkout -b develop origin/develop
-sudo docker build . -t acrwebdev/web-login-register
-sudo docker run -p 8080:3000 --restart=always --name=web-login-register -d acrwebdev/web-login-register
+sudo docker build . -t acrwebdev/realtor-lease-platform
+sudo docker run -p 8080:3000 --restart=always --name=realtor-lease-platform -d acrwebdev/realtor-lease-platform
