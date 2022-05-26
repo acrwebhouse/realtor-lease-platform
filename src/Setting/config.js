c826d722934272dd7259945e5ddcd00792b6410a
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-05-26 19:11:06 +0800

    config alpha

--- src/Setting/config.js
+++ src/Setting/config.js
@@ -1,10 +1,10 @@
 
 const config = {
-    'base_URL_Auth' : 'http://34.81.209.11:3000',
-    'base_URL_User' : 'http://34.81.209.11:4000',
-    'base_URL_House' : 'http://34.81.209.11:5000',
-    'base_URL_Collect' : 'http://34.81.209.11:6001',
-    'contactPhone' : '0909123456',
-    'contactMail' : 'acr.webhouse@gmail.com',
+    'base_URL_Auth' : 'http://104.199.204.162:3000',
+    'base_URL_User' : 'http://104.199.204.162:4000',
+    'base_URL_House' : 'http://104.199.204.162:5000',
+    'base_URL_Collect' : 'http://104.199.204.162:6001',
+    'contactPhone' : '0912951085',
+    'contactMail' : 'acr.realtorplatform@gmail.com',
 }
 export {config}
