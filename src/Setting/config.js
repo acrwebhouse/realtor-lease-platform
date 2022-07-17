fccc40f4f6f1a17b54d742550e85d0b3997895c4
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-07-17 21:40:27 +0800

    change config

--- src/Setting/config.js
+++ src/Setting/config.js
@@ -1,10 +1,19 @@
 
 const config = {
-    'base_URL_Auth' : 'http://34.81.209.11:3000',
-    'base_URL_User' : 'http://34.81.209.11:4000',
-    'base_URL_House' : 'http://34.81.209.11:5000',
-    'base_URL_Collect' : 'http://34.81.209.11:6001',
+    'base_URL_Auth' : 'https://matchrentdev.com/auth-service',
+    'base_URL_User' : 'https://matchrentdev.com/user-service',
+    'base_URL_House' : 'https://matchrentdev.com/house-service',
+    'base_URL_Collect' : 'https://matchrentdev.com/collect-service',
     'contactPhone' : '0909123456',
     'contactMail' : 'acr.webhouse@gmail.com',
 }
+
+// const config = {
+//     'base_URL_Auth' : 'http://34.81.209.11:3000',
+//     'base_URL_User' : 'http://34.81.209.11:4000',
+//     'base_URL_House' : 'http://34.81.209.11:5000',
+//     'base_URL_Collect' : 'http://34.81.209.11:6001',
+//     'contactPhone' : '0909123456',
+//     'contactMail' : 'acr.webhouse@gmail.com',
+// }
 export {config}
