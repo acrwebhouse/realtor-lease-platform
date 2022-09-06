786ab728f9f3228944516e5be88b7e2eb3559323
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-09-06 19:56:21 +0800

    add ReserveHouseDetail and ReserveHouseList

---
+++ src/Components/ReserveHouseList.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import {Table, Space, Radio, Button, Image, Input, Select, Divider, Row, Col, DatePicker, message, Alert, Checkbox, Result} from "antd";
+import cookie from 'react-cookies'
+import {UserAxios} from './axiosApi'
+import jwt_decode from "jwt-decode";
+import moment from 'moment';
+
+
+const ReserveHouseList = (props) => {
+
+    return (
+        <div>
+            
+            ReserveHouseList page
+            
+        </div>
+    );
+};
+
+export default ReserveHouseList;
