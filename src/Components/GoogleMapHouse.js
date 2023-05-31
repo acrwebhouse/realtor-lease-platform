80ea54b5e248d0ef310f752e08311692b12fbc91
chengyen.chiang <chengyen.chiang@leediancn.com>
2023-05-31 23:36:24 +0800

    hot fix deploy google map use lat lon

--- src/Components/GoogleMapHouse.js
+++ src/Components/GoogleMapHouse.js
@@ -18,7 +18,13 @@ const GoogleMapHouse = (props) => {
     const [init, setInit] = useState(true);
     const [house, setHouse ] = useState(null);
     const [MapSource, setMapSource] = useState('')
-    // console.log(house)
+    const [address] = useState(
+        {
+            lat : '',
+            lon : ""
+        }
+    )
+    // console.log(house, address.lat.length)
 
     const getHouse = () => {
         let reqUrl = `${houseListUrl}?id=${id}&&isDelete=false`
@@ -36,6 +42,8 @@ const GoogleMapHouse = (props) => {
         if(response.data.status){
             const data = response.data.data
             setHouse(data)
+            address.lat = response.data.data.lat
+            address.lon = response.data.data.lon
         }
     }
 
@@ -48,38 +56,55 @@ const GoogleMapHouse = (props) => {
 
     useEffect(()=>{
         if(!init && house) {
-            console.log(house.houseNumber.alley, house.houseNumber.lane)
-            let houseExtraData = '';
-            if(house.houseNumber.lane) {
-                if(house.houseNumber.alley) {
-                    houseExtraData = house.address+house.houseNumber.lane+'巷' + house.houseNumber.alley+'弄' + house.houseNumber.number1+'號'
-                } else {
-                    houseExtraData = house.address+house.houseNumber.lane+'巷' + house.houseNumber.number1+'號'
-                }
+            if(address.lat && address.lat.length > 0) {
+                setMapSource(defaultMapSource+address.lat+", "+address.lon)
             } else {
-                houseExtraData = house.address+ house.houseNumber.number1+'號'
+                setMapSource(defaultMapSource+house.address)
             }
-            setMapSource(defaultMapSource+houseExtraData)
+
             console.log(MapSource)
-            console.log(house.address)
-            console.log(houseExtraData)
             const main = document.getElementById('main')
             const mainWidth = main.offsetWidth
             main.style.height = mainWidth + 'px'
         }
     }, [init, house])
+    // useEffect(()=>{
+    //     if(!init && house) {
+    //         console.log(house.houseNumber.alley, house.houseNumber.lane)
+    //         let houseExtraData = '';
+    //         if(house.houseNumber.lane) {
+    //             if(house.houseNumber.alley) {
+    //                 houseExtraData = house.address+house.houseNumber.lane+'巷' + house.houseNumber.alley+'弄' + house.houseNumber.number1+'號'
+    //             } else {
+    //                 houseExtraData = house.address+house.houseNumber.lane+'巷' + house.houseNumber.number1+'號'
+    //             }
+    //         } else {
+    //             houseExtraData = house.address+ house.houseNumber.number1+'號'
+    //         }
+    //         setMapSource(defaultMapSource+houseExtraData)
+    //         console.log(MapSource)
+    //         console.log(house.address)
+    //         console.log(houseExtraData)
+    //         const main = document.getElementById('main')
+    //         const mainWidth = main.offsetWidth
+    //         main.style.height = mainWidth + 'px'
+    //     }
+    // }, [init, house])
 
+    console.log(house, address, MapSource)
     return (
-                <iframe
-                    id="main"
-                    width="100%"
-                    style={{border: "0"}}
-                    loading="lazy"
-                    allowFullScreen
-                    referrerPolicy="no-referrer-when-downgrade"
-                    src={MapSource}
-                />
-
+        <div style={{width: "100%", overflow: "hidden", height: "100%"}}>
+            <iframe
+                id="main"
+                width="600"
+                style={{border: "0", marginTop: "-125px"}}
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src={MapSource}
+                // src={'https://www.google.com/maps/embed/v1/place?key=AIzaSyB7bluQMbii0q2B5v7o6SABJRgddKW8GYE&q=25.0564179, 121.5531218'}
+            />
+        </div>
     );
 };
 
